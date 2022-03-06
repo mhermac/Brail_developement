797bfafe3558bbc138a54acc6d28cc9f017d2a8e
Mher Simonyan <m.simonyan@innoverse.tech>
2022-03-06 20:42:58 +0400

    test

--- lib/routing/application_route_link.dart
+++ lib/routing/application_route_link.dart
@@ -8,4 +8,7 @@ abstract class ApplicationRouteLink{
   static const DRIVER_ADD_EDIT_OFFER = '/DRIVER_ADD_EDIT_OFFER';
   static const DRIVER_OFFERS = '/DRIVER_OFFERS';
   static const DRIVER_CURRENT_ORDER = '/DRIVER_CURRENT_ORDER';
+  static const DRIVER_CURRENT = '/DRIVER_CURRENT';
+  static const DRIVER_test = '/DRIVER_test';
+
 }
